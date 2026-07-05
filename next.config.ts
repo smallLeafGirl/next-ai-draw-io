ef70fb59a1f0ac5d7110d79d6941345b2fe024d6
tom-8080 <51500974+smallLeafGirl@users.noreply.github.com>
2026-07-05 21:41:18 +0800

    Update next.config.ts
    更改文件

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: { unoptimized: true },
   trailingSlash: true,
   eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }
-}
-module.exports = nextConfig
+  typescript: { ignoreBuildErrors: true },
+};
+
+export default nextConfig;
